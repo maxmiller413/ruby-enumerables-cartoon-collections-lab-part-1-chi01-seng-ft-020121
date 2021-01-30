dc7ad5864f3bef79f161a5f4ea730a724445917e
Max Miller <maxmiller413@gmail.com>
2021-01-30 03:31:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,9 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   
-  array.each_with_index do |character|
+  array.each_with_index do |character, index|
+    puts "index. #{character}"
     
   end  
- 
+  array 
 end
